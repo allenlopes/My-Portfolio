8e64fed734b8afc214158c05660650cd7fb6f867
sudoallenlopes23 <allenlopes10@gmail.com>
2023-08-04 23:58:53 +0530

    In components/header/CTA.jsx file modified by editing the link

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href='https://drive.google.com/file/d/1k1desQ_9JcVBtRgM0OFF36sm1yAzxGnC/view?usp=sharing' download className='btn' target='_blank' rel="noreferrer">Download Resume</a>
+        <a href='https://drive.google.com/file/d/1DZL9TLyfRowLrQumEW-ycZbdN9z_XQ7T/view?usp=sharing' download className='btn' target='_blank' rel="noreferrer">Download Resume</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
